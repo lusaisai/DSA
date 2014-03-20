15dbbd32059d0ca986c22cdd556adb96d634589e
lusaisai <lusaisai@163.com>
2014-03-21 03:28:47 +0800

    set DSA.rb

--- lib/DSA.rb
+++ lib/DSA.rb
@@ -1,9 +1,9 @@
-require '../lib/DSA/version'
-require '../lib/DSA/algorithm'
-require '../lib/DSA/stack_and_queue'
-require '../lib/DSA/list'
-require '../lib/DSA/priority_queue'
-require '../lib/DSA/binary_search_tree'
+require_relative 'DSA/version'
+require_relative 'DSA/algorithm'
+require_relative 'DSA/stack_and_queue'
+require_relative 'DSA/list'
+require_relative 'DSA/priority_queue'
+require_relative 'DSA/binary_search_tree'
 
 module DSA
   # Your code goes here...
